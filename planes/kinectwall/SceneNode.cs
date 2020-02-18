268bdc44173adc4ee61c343e775ef3b89bb6edca
Shane Morrison <shanemor@gmail.com>
2020-02-18 12:35:33 -0500

    from script

---
+++ planes/kinectwall/SceneNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public abstract class SceneNode
+    {
+        protected string name;
+        public string Name => name;
+        public abstract IEnumerable<SceneNode> Nodes { get; }
+
+        protected SceneNode(string _n)
+        {
+            name = _n;
+        }
+    }
+}
